97fd8a679621e4718b9dd33d424c0b323cd6454c
Ekhel <blackmanskill@gmail.com>
2019-01-14 20:02:57 +0900

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
+
+const config = {
+  apiKey: 'AIzaSyBCX5GLfFFs0QRX6OEeW_Sj1iGQnaO8H9c',
+  authDomain: 'geotracker1-ca999.firebaseapp.com',
+  databaseURL: 'https://geotracker1-ca999.firebaseio.com',
+  projectId: 'geotracker1-ca999',
+  storageBucket: 'geotracker1-ca999.appspot.com',
+  messagingSenderId: "40237978929"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,6 +28,8 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    firebase.initializeApp(config);
   }
 }
 
